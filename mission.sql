4ed718f5d66d7a820c6aa4f6a59f337538ee08e6
Summer <113702628+sum00er@users.noreply.github.com>
2023-03-06 20:24:16 +0800

    Update mission.sql

--- mission.sql
+++ mission.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `mission` (
   `identifier` varchar(50) DEFAULT NULL,
   `mission_id` int(10) unsigned DEFAULT NULL,
   `max` int(10) unsigned DEFAULT NULL,
-  `porgress` int(10) unsigned DEFAULT 0,
+  `progress` int(10) unsigned DEFAULT 0,
   `finish` tinyint(3) unsigned DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
